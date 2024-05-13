85d15d66bca2fc98b00212943881464d546f1730
Bearindigo105 <m.muthusubhash@gmail.com>
2024-05-12 21:18:20 -0400

    modified:   .gitignore 	modified:   src/App.java

--- src/App.java
+++ src/App.java
@@ -1,18 +1,32 @@
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Sphere;
 import javafx.stage.Stage;
 
 public class App extends Application {
-    public static void main(String[] args) throws Exception {
-        launch(args);
-    }
+
+    public static final int WIDTH = 1600;
+    public static final int HEIGHT = 900;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        StackPane root = new StackPane();
+        Sphere sphere = new Sphere(50);
+        
+        Group group = new Group();
+        group.getChildren().add(sphere);
+        
+        Scene scene = new Scene(group, WIDTH, HEIGHT);
+
+        sphere.translateXProperty().set(WIDTH / 2);
+        sphere.translateYProperty().set(HEIGHT / 2);
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
     }
 }
