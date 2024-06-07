f5bab96ce7d506198f3bed2bd45be38a8304835c
F1R3PH03N1X <psureshkayalvizhi26@student.gvsd.org>
2024-06-07 08:44:15 -0400

    modified:   src/main/java/App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,23 @@
 package main.java;
 
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Point3D;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.SceneAntialiasing;
 import javafx.scene.SubScene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.*;
 import javafx.scene.shape.Box;
 import javafx.scene.shape.Rectangle;
@@ -104,12 +112,37 @@ public class App extends Application {
                 }
             });
         });
-        Group titleGroup = new Group();
-        Scene titleScene = new Scene(titleGroup, WIDTH, HEIGHT);
+
+        Pane titlePane = new Pane();
+        Scene titleScene = new Scene(titlePane, WIDTH, HEIGHT);
+
+        titlePane.setStyle("-fx-background-color: black;");
+        ImageView title = new ImageView();
+        title.setImage(new javafx.scene.image.Image("file:src/main/resources/textures/game.png"));
+        title.relocate((WIDTH / 2) - 175, (HEIGHT / 2) - 100);
+        JButton playButton = new JButton();
+        JButton playButton2 = new JButton();
+        playButton.set(new java.awt.Image("file:src/main/resources/textures/playbutton.png"));
+        playButton.relocate((WIDTH / 2) - 70, (HEIGHT / 2) + 100);
+        ImageView playButton1 = new ImageView();
+        playButton2.setImage(new Image("file:src/main/resources/textures/playbutton1.png"));
+        playButton2.relocate((WIDTH / 2) - 70, (HEIGHT / 2) + 100);
+        
+        titlePane.getChildren().addAll(title); 
+        titlePane.getChildren().addAll(playButton); 
+
 
         primaryStage.setTitle("caveDEAD");
-        primaryStage.setScene(gameScene);
+        primaryStage.setScene(titleScene);
         primaryStage.show();
+
+        primaryStage.addEventHandler(KeyEvent.mou, event -> {
+            Platform.runLater(() -> {
+                switch (event.getCode()) {
+                    
+                }
+            });
+        });
     }
 
     public static void main(String[] args) throws Exception {
